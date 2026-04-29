9d84c6663ac427e3e019c85d4ba1b821126606d9
tygopal45 <jeegopal835@gmail.com>
2026-04-29 11:55:20 +0530

    Step 1 - added indexing

---
+++ main.py
@@ -0,0 +1,34 @@
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+
+# Step 1(a) : Indexing(Document Ingestion)
+
+video_id = "Gfr50f6ZBvo"
+
+try:
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=["en", "en-US"])
+
+    transcript = " ".join(chunk["text"] for chunk in transcript_list)
+
+except TranscriptsDisabled:
+    print("Transcripts are disabled")
+
+
+
+# Step 1(b) : Indexing(Text Chunking)
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = splitter.create_documents([transcript])
+
+# Step 1(c) & 1(d): Indexing(Embedding Creation and Storing in Vector Database)
+embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+
+vector_store = FAISS.from_documents(chunks, embeddings)
+
+
+
+
